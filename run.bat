9b474244a78aa53cfd231cd607bb7d13f04ae6e1
pacbypass <pacbypass@gmail.com>
2021-02-07 19:32:26 +0100

    cleanup

--- run.bat
+++
@@ -1,4 +0,0 @@
-cargo run
-@echo off
-bash -c "sshpass -p fuck ssh nigger@192.168.1.150 'rm -rf /home/nigger/chocolate_milk/pxe/*'"
-bash -c "sshpass -p fuck scp ./pxe/* nigger@192.168.1.150:chocolate_milk/pxe" 
